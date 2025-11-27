65175a3882049a97ac2bba91ddb13e91b325bace
Craig <craig.hickman@ukaea.uk>
2025-11-27 18:46:43 +0000

    Made test more generic

--- test/proof_of_concept_test.dart
+++ test/proof_of_concept_test.dart
@@ -1,25 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-final globalValueNotifier = ValueNotifier(42);
-
 Text buildMyWidget<B,T,W>(B _, T value, W _) {
   return Text('$value');
 }
 
-MaterialApp bootstrap() => MaterialApp(
+MaterialApp bootstrap(ValueNotifier valueNotifier) => MaterialApp(
   home: ValueListenableBuilder(
-    valueListenable: globalValueNotifier,
+    valueListenable: valueNotifier,
     builder: buildMyWidget,
   ),
 );
 
 void main() {
   testWidgets('Proof of Concept Test', (WidgetTester tester) async {
-    final app = bootstrap();
+    final valueNotifier = ValueNotifier(42);
+    final app = bootstrap(valueNotifier);
     await tester.pumpWidget(app);
     expect(find.text('42'), findsOneWidget, reason: 'Initial value should be 42');
-    globalValueNotifier.value = 43;
+    valueNotifier.value = 43;
     expect(find.text('43'), findsNothing, reason: 'Value should not update until widget is pumped again');
     await tester.pump();
     expect(find.text('43'), findsOneWidget, reason: 'Value should update to 43 after pumping the widget again');
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,71 +6,58 @@ import 'package:flutter_app_layered_architecture_with_go_router/frontend/ui_page
 import 'package:flutter_app_layered_architecture_with_go_router/frontend/controller_interfaces.dart';
 import 'package:flutter_app_layered_architecture_with_go_router/backend/domain_model.dart';
 
-final globalValueNotifier = ValueNotifier<AppModel>(AppModel(42));
-final globalController = MockController();
-
-void main() {
-  testWidgets('CounterUpView smoke test', (WidgetTester tester) async {
-    var testLog = <String>[];
-    final app = bootstrap(() => CounterUpView(controller: globalController), testLog);
-    await tester.pumpWidget(app);
-    globalValueNotifier.value = AppModel(43);
-    await tester.pumpWidget(app);
-    expect(find.text('Value: 43'), findsOneWidget);
-  });
+CounterUpView buildMyWidget(ValueNotifier<AppModel> valueNotifier) {
+  return CounterUpView(controller: MockController(valueNotifier));
 }
 
-/// Wraps the given child widget with the minimal functionality needed to test UI pages:
-MaterialApp bootstrap<T extends Widget>(T Function() childBuilder, List<String> debugInfo) {
-  //Wrap the child in a ValueListenableBuilder so it is rebuilt whenever the global valueNotifier changes
-  final childListenableBuilder = ValueListenableBuilder(
-    valueListenable: globalValueNotifier,
-    builder: (_, _, _) {
-      debugInfo.log("Rebuilding ${T.runtimeType}");
-      return childBuilder();
-    }
-  );
+ValueListenableBuilder _bootstrap(ValueNotifier<AppModel> valueNotifier) => ValueListenableBuilder(
+  valueListenable: valueNotifier,
+  builder: (_,_,_) => buildMyWidget(valueNotifier),
+);
 
-  //Wrap the childListenableBuilder in a GoRouter so it's build context contains a GoRouter object (required because UI pages use context.go)
-  final routableChild = GoRouter(
+MaterialApp bootstrapApp(ValueNotifier<AppModel> valueNotifier) => MaterialApp(
+  home: _bootstrap(valueNotifier),
+);
+
+MaterialApp bootstrapRouter(ValueNotifier<AppModel> valueNotifier) => MaterialApp.router(
+  routerConfig: GoRouter(
+    initialLocation: '/',
     routes: [
       GoRoute(
         path: '/',
-        builder: (_, _) {
-          debugInfo.log("Building ${childListenableBuilder.runtimeType}}");
-          return childListenableBuilder;
-        },
+        builder: (_,_) => _bootstrap(valueNotifier),
       ),
-    ],
-  );
-  //Wrap the routable child in a MaterialApp so it has the necessary Material dependencies (Scaffold, AppBar, Theme.of, Navigator, MediaQuery, MaterialLocalizations, etc)
-  return MaterialApp.router(
-   routerConfig: routableChild,
-  );
+    ]
+  )
+);
+
+void main() {
+  testWidgets('CounterUpView smoke test', (WidgetTester tester) async {
+    final valueNotifier = ValueNotifier<AppModel>(AppModel(42));
+    final app = bootstrapRouter(valueNotifier);
+    await tester.pumpWidget(app);
+    expect(find.text('Value: 42'), findsOneWidget);
+  });
 }
 
-/// The mock controller uses the global valueNotifier for state management and reactivity.
+/// The mock controller uses the valueNotifier for state management and reactivity.
 /// The real controllers are dumb, and just call the onModelChanged callback when they mutate the model.
 /// The reactivity in the real app is handled in the router, where the ValueNotifier is updated when the controller calls onModelChanged.
 class MockController implements IUpController, IDownController {
     
+  final ValueNotifier<AppModel> valueNotifier;
+  MockController(this.valueNotifier);
+
   @override
-  AppModel get model => globalValueNotifier.value;
+  AppModel get model => valueNotifier.value;
 
   @override
   void increment() {
-    globalValueNotifier.value = AppModel(model.counter + 1);
+    valueNotifier.value = AppModel(model.counter + 1);
   }
 
   @override
   void decrement() {
-    globalValueNotifier.value = AppModel(model.counter - 1);
-  }
-}
-
-extension on List<String> {
-  void log(String message) {
-    this.add(message);
-    debugPrint(message);
+    valueNotifier.value = AppModel(model.counter - 1);
   }
 }
